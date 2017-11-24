b673db7393d3030b60de61ec78d9d913332ade13
Peter Klenner <peterklenner@gmx.de>
2017-11-24 08:00:23 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,4 @@
+# py-encrypt-pdf
+
+`py-encrypt-pdf` wraps `gswin64c` and is a convenience function 
+for setting user and ower password for pdfs.
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup( 
+    name = 'py-encrypt-pdf',
+    version = '0.1',
+    description = 'Ghostscript wrapper for convenient PDF password encryption',
+    packages=find_packages())
