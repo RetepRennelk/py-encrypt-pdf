953a87aca87eae803914fe66f5d4d002915e5611
Peter Klenner <peterklenner@gmx.de>
2017-11-24 08:29:28 +0100

    Modified setuptools to provide commandline exe 'py-encrypt-pdf'.

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ setup(
     name = 'py-encrypt-pdf',
     version = '0.1',
     description = 'Ghostscript wrapper for convenient PDF password encryption',
-    packages=find_packages())
+    packages=find_packages(),
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'py-encrypt-pdf=py_encrypt_pdf.py_encrypt_pdf:encrypt']})
